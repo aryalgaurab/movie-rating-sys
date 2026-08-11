80d5045c8545fa77ede45b9b2ee1ad8278a1c374
aryalgaurab <aryalgaurab70@gmail.com>
2026-08-11 10:58:53 +0545

    fix the delpoyment

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,14 +1,24 @@
+import "server-only";
 import { PrismaClient } from "../app/generated/prisma/client";
 import { Pool } from "pg";
 import { PrismaPg } from "@prisma/adapter-pg"
 
 const globalForPrisma = globalThis as unknown as {
-  prisma: PrismaClient;
+  prisma: PrismaClient | undefined;
+  pool: Pool | undefined;
 };
 
-const pool = new Pool({connectionString: process.env.DATABASE_URL})
-const adapter = new PrismaPg(pool)
+if (!globalForPrisma.pool) {
+  globalForPrisma.pool = new Pool({
+    connectionString: process.env.DATABASE_URL,
+  });
+}
 
-export const prisma = globalForPrisma.prisma ?? new PrismaClient({adapter});
+if (!globalForPrisma.prisma) {
+  const adapter = new PrismaPg(globalForPrisma.pool);
+  globalForPrisma.prisma = new PrismaClient({ adapter });
+}
+
+export const prisma = globalForPrisma.prisma;
 
 if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
