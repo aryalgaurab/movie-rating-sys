ad41f8b2c51fa49aa78b9ea1490b9c2b8f6b34e4
aryalgaurab <aryalgaurab70@gmail.com>
2026-08-11 12:01:53 +0545

    disable turbopack for prod build

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,11 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   serverExternalPackages: ["@prisma/adapter-pg", "pg"],
+
+  // to disable Turbopack and use standard Webpack compilation for production builds!
+  webpack: (config) => {
+    return config;
+  },
 };
 
 export default nextConfig;
