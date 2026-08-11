b92495f724751e29b95df2d397e53106fb991079
aryalgaurab <aryalgaurab70@gmail.com>
2026-08-11 12:08:35 +0545

    add --webpack in build script

--- next.config.ts
+++ next.config.ts
@@ -4,10 +4,6 @@ const nextConfig: NextConfig = {
   /* config options here */
   serverExternalPackages: ["@prisma/adapter-pg", "pg"],
 
-  // to disable Turbopack and use standard Webpack compilation for production builds!
-  webpack: (config) => {
-    return config;
-  },
 };
 
 export default nextConfig;
