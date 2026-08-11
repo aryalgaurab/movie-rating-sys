4865c7889825afb6130cfa597dea12aee4dbe313
aryalgaurab <aryalgaurab70@gmail.com>
2026-08-11 13:23:31 +0545

    adjusted all files to configure build error

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,6 +1,6 @@
 import "server-only";
-import { PrismaClient } from "@/generated/prisma/client";
-import { PrismaPg } from "@prisma/adapter-pg"
+import { PrismaClient } from "@prisma/client";
+import { PrismaPg } from "@prisma/adapter-pg";
 
 const globalForPrisma = globalThis as unknown as {
   prisma: PrismaClient | undefined;
